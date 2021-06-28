5474a675ded57ad6f852b389cea4c352c001e5d1
Anton Belyaev <belyash23@inbox.ru>
2021-06-28 13:18:08 +0400

    complete task#5

---
+++ js/5/languages.js
@@ -0,0 +1,195 @@
+export const languages = {
+    "af": {
+        "name": "Afrikaans",
+        "nativeName": "Afrikaans",
+    },
+    "ar": {
+        "name": "Arabic",
+        "nativeName": "العربية",
+        "isRTL": true,
+    },
+    "az": {
+        "name": "Azerbaijani",
+        "nativeName": "azərbaycan dili",
+    },
+    "be": {
+        "name": "Belarusian",
+        "nativeName": "Беларуская",
+    },
+    "bg": {
+        "name": "Bulgarian",
+        "nativeName": "български език",
+    },
+    "ca": {
+        "name": "Catalan",
+        "nativeName": "Català",
+    },
+    "hr": {
+        "name": "Croatian",
+        "nativeName": "hrvatski",
+    },
+    "cs": {
+        "name": "Czech",
+        "nativeName": "česky",
+    },
+    "da": {
+        "name": "Danish",
+        "nativeName": "dansk",
+    },
+    "nl": {
+        "name": "Dutch",
+        "nativeName": "Nederlands",
+    },
+    "en": {
+        "name": "English",
+        "nativeName": "English",
+        "count": 1
+    },
+    "eo": {
+        "name": "Esperanto",
+        "nativeName": "Esperanto",
+    },
+    "et": {
+        "name": "Estonian",
+        "nativeName": "eesti",
+    },
+    "fo": {
+        "name": "Faroese",
+        "nativeName": "føroyskt",
+    },
+    "fi": {
+        "name": "Finnish",
+        "nativeName": "suomi",
+    },
+    "fr": {
+        "name": "French",
+        "nativeName": "français",
+    },
+    "gl": {
+        "name": "Galician",
+        "nativeName": "Galego",
+    },
+    "ka": {
+        "name": "Georgian",
+        "nativeName": "ქართული",
+    },
+    "de": {
+        "name": "German",
+        "nativeName": "Deutsch",
+    },
+    "el": {
+        "name": "Greek",
+        "nativeName": "Ελληνικά",
+    },
+    "he": {
+        "name": "Hebrew",
+        "nativeName": "עברית",
+        "isRTL": true
+        ,
+    },
+    "hi": {
+        "name": "Hindi",
+        "nativeName": "हिन्दी, हिंदी",
+    },
+    "hu": {
+        "name": "Hungarian",
+        "nativeName": "Magyar",
+    },
+    "id": {
+        "name": "Indonesian",
+        "nativeName": "Bahasa Indonesia",
+    },
+    "is": {
+        "name": "Icelandic",
+        "nativeName": "Íslenska",
+    },
+    "it": {
+        "name": "Italian",
+        "nativeName": "Italiano",
+    },
+    "ja": {
+        "name": "Japanese",
+        "nativeName": "日本語",
+    },
+    "kk": {
+        "name": "Kazakh",
+        "nativeName": "Қазақ тілі",
+    },
+    "km": {
+        "name": "Khmer",
+        "nativeName": "ភាសាខ្មែរ",
+    },
+    "ko": {
+        "name": "Korean",
+        "nativeName": "한국어",
+    },
+    "lb": {
+        "name": "Luxembourgish",
+        "nativeName": "Lëtzebuergesch",
+    },
+    "lt": {
+        "name": "Lithuanian",
+        "nativeName": "lietuvių kalba",
+    },
+    "lv": {
+        "name": "Latvian",
+        "nativeName": "latviešu valoda",
+    },
+    "mk": {
+        "name": "Macedonian",
+        "nativeName": "македонски јазик",
+    },
+    "ml": {
+        "name": "Malayalam",
+        "nativeName": "മലയാളം",
+    },
+    "nb": {
+        "name": "Norwegian Bokmål",
+        "nativeName": "Norsk bokmål",
+    },
+    "pl": {
+        "name": "Polish",
+        "nativeName": "polski",
+    },
+
+    "pt": {
+        "name": "Portuguese",
+        "nativeName": "Português",
+    },
+
+    "rm": {
+        "name": "Romansh",
+        "nativeName": "rumantsch",
+    },
+
+    "ro": {
+        "name": "Romanian",
+        "nativeName": "română",
+    },
+    "ru": {
+        "name": "Russian",
+        "nativeName": "русский язык",
+    },
+    "sk": {
+        "name": "Slovak",
+        "nativeName": "slovenčina",
+    },
+    "sl": {
+        "name": "Slovene",
+        "nativeName": "slovenščina",
+    },
+    "sv": {
+        "name": "Swedish",
+        "nativeName": "svenska",
+    },
+    "th": {
+        "name": "Thai",
+        "nativeName": "ไทย",
+    },
+    "uk": {
+        "name": "Ukrainian",
+        "nativeName": "українська",
+    },
+}
+
+export default languages;
---
+++ js/5/script.js
@@ -0,0 +1,61 @@
+import languages from './languages.js';
+
+async function getLocals() {
+    for (let key in languages) {
+        await import(`./i18n/datepicker-${key}.js`);
+        const option = $(`<option>`).attr('value', key).text(languages[key].nativeName);
+        if (key === 'en') {
+            option.attr('selected', true);
+            dateFormat = $.datepicker.regional['en'].dateFormat;
+        }
+        $('#locale').append(option);
+    }
+}
+function getDate(element) {
+    let date = null;
+    try {
+        date = $.datepicker.parseDate(dateFormat, element.value);
+    } catch( error ) {
+        date = null;
+    }
+    return date;
+}
+function highlightDay(date) {
+    if ((dateStart != null || dateEnd != null) && (dateStart <= date || date <= dateEnd)) {
+        return [true, "text-primary"];
+    }
+    return [true]
+}
+
+const datepicker = $('.datepicker');
+let dateFormat = null;
+let dateStart = null;
+let dateEnd= null;
+
+getLocals().then(() => {
+    const start = $(".datepicker-start").datepicker({
+        beforeShowDay: highlightDay
+    }).datepicker('option', $.datepicker.regional['en']).on("change", function () {
+        dateStart = getDate(this);
+        end.datepicker("option", "minDate", getDate(this));
+    })
+    const end = $(".datepicker-end").datepicker({
+        beforeShowDay: highlightDay
+    }).datepicker('option', $.datepicker.regional['en']).on("change", function () {
+        dateEnd = getDate(this);
+        start.datepicker("option", "maxDate", getDate(this));
+    });
+    $('#locale').on("change", function () {
+        datepicker.datepicker("option", $.datepicker.regional[$(this).val()]);
+        dateFormat = $.datepicker.regional[$(this).val()].dateFormat;
+    });
+});
+
+$('.form').on('submit', e => {
+    e.preventDefault();
+    const error = $('.error');
+    error.text('');
+    if(!dateStart && !dateEnd) {
+        error.text('Необходимо задать дату начала или дату конца');
+    }
+})
